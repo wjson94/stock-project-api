62b76a1d01d4597644e248af722f27208f9643c2
wjson94 <ohjr008@gmail.com>
2026-06-15 22:57:18 +0900

    Stock Project JWT 인증 기능 추가

--- src/main/java/com/wjproject/stockproject/global/config/SwaggerConfig.java
+++ src/main/java/com/wjproject/stockproject/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.wjproject.stockproject.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,10 +11,21 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI testOpenAPI(){
+    public OpenAPI CustomOpenAPI(){
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("BearerAuth");
+
         return new OpenAPI()
                 .info(new Info().title("Stock Project API 문서")
                         .description("테스트 API")
-                        .version("1.0"));
+                        .version("1.0"))
+                        .addSecurityItem(securityRequirement)
+                        .schemaRequirement("BearerAuth", securityScheme);
     }
 }

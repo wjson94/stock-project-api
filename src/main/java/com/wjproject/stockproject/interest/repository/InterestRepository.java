b2ad0f34264bec25f2ad144e19c9350c31514429
wjson94 <ohjr008@gmail.com>
2026-06-17 05:05:41 +0900

    Stock Project 관심 종목 리스트 조회 및 조회 응답 포맷 기능 생성

--- src/main/java/com/wjproject/stockproject/interest/repository/InterestRepository.java
+++ src/main/java/com/wjproject/stockproject/interest/repository/InterestRepository.java
@@ -3,12 +3,13 @@ package com.wjproject.stockproject.interest.repository;
 import com.wjproject.stockproject.interest.entity.InterestStock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface InterestRepository extends JpaRepository<InterestStock, Long> {
 
-    boolean existsByUserSeqAndStockCode(
-            Long userSeq,
-            String stockCode
-    );
+    List<InterestStock> findAllByUserSeqOrderByCreatedAtDesc(Long userSeq);
+
+    boolean existsByUserSeqAndStockCode(Long userSeq, String stockCode);
 
     int deleteByUserSeqAndStockCode(Long userSeq, String stockCode);
 }
--- src/main/java/com/wjproject/stockproject/interest/service/InterestService.java
+++ src/main/java/com/wjproject/stockproject/interest/service/InterestService.java
@@ -2,11 +2,14 @@ package com.wjproject.stockproject.interest.service;
 
 import com.wjproject.stockproject.global.common.exception.CustomException;
 import com.wjproject.stockproject.global.common.response.ErrorCode;
+import com.wjproject.stockproject.interest.dto.response.InterestStockListResponse;
 import com.wjproject.stockproject.interest.entity.InterestStock;
 import com.wjproject.stockproject.interest.repository.InterestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,11 +18,19 @@ public class InterestService {
 
     private final InterestRepository interestRepository;
 
+    public InterestStockListResponse getInterestStocks(Long userSeq) {
+
+        List<InterestStock> interestStocks =
+                interestRepository.findAllByUserSeqOrderByCreatedAtDesc(userSeq);
+
+        return InterestStockListResponse.from(interestStocks);
+    }
+
     @Transactional
     public void addInterestStock(Long userSeq, String stockCode) {
 
         if (interestRepository.existsByUserSeqAndStockCode(userSeq, stockCode)) {
-            throw new CustomException(ErrorCode.STOCK_DUPLICATE_INTEREST);
+            throw new CustomException(ErrorCode.STOCK_DUPLICATE);
         }
 
         InterestStock interestStock = InterestStock.builder()
